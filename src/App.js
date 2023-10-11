015f52e19b1cf68b59d23898e9160bc39c64f65a
yordigenet <105040859+yordigenet@users.noreply.github.com>
2023-10-11 06:05:57 -0700

    Added a link to github

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <h1>React Weather App</h1>
         <Weather/>
+        <p className='footer'>Coded by <a href="https://www.shecodes.io/graduates/83359-yordanos-girma" target='blank'>Yordanos Girma</a> and open-source at <a href="https://github.com/yordigenet/react-weather-app" target='blank'>Github</a></p>
       </header>
     </div>
   );
